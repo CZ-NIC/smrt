cf3c031242fe6e37ee18bff7496d55c7465df6ae
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2014-12-02 13:11:10 +0100

    Licence (GPL3)

--- src/automaton.c
+++ src/automaton.c
@@ -1,3 +1,21 @@
+/*
+ * SMRTd ‒ daemon to initialize the Small Modem for Router Turris
+ * Copyright (C) 2014 CZ.NIC, z.s.p.o. <http://www.nic.cz>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "automaton.h"
 #include "proto_const.h"
 #include "util.h"
